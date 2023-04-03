ee53fa23e876699fa717f4a2edaf189f98b75e16
Franco <blancofranco205@gmail.com>
2023-04-03 15:35:20 -0300

    index-html

--- proyecto_final/core/models.py
+++ proyecto_final/core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Futbol(models.Model):
+    nombre = models.CharField(max_length=20)
+    nro_equipos = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.id} - {self.nombre} - {self.nro_equipos}"
---
+++ proyecto_final/core/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path, include 
+from core.views import inicio
+urlpatterns = [
+    path("", inicio, name="index"),
+]
