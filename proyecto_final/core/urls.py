01112cb28f779b0e2a30961b0c28f9290e026223
Franco <blancofranco205@gmail.com>
2023-04-04 11:15:33 -0300

    Herencia de html

--- proyecto_final/core/urls.py
+++ proyecto_final/core/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include 
-from core.views import inicio
+from core.views import inicio, mostrar_futbol
 urlpatterns = [
     path("", inicio, name="index"),
+    path("mostrar_futbol", mostrar_futbol, name="futbol"),
 ]
--- proyecto_final/core/views.py
+++ proyecto_final/core/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def inicio(request):
-    return render(request, "core/index.html")
+    return render(request, "core/base.html")
+
+def mostrar_futbol(request):
+    return render(request, "core/mostrar_futbol.html")
