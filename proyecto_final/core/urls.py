1f67dfadcbb1a3bb48fe6a89431060b16b7d7e57
Franco <blancofranco205@gmail.com>
2023-04-09 11:30:42 -0300

    Agregado de templates

--- proyecto_final/core/urls.py
+++ proyecto_final/core/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include 
-from core.views import inicio, mostrar_futbol
+from core.views import inicio, mostrar_futbol, editar_futbol, eliminar_futbol, agregar_futbol
 urlpatterns = [
     path("", inicio, name="index"),
-    path("mostrar_futbol", mostrar_futbol, name="futbol"),
+    path("mostrar_futbol/", mostrar_futbol, name="mostrar_futbol"),
+    path("agregar_futbol/", agregar_futbol, name="agregar_futbol"),
+    path("editar_futbol/", editar_futbol, name="editar_futbol"),
+    path("eliminar_futbol/", eliminar_futbol, name="eliminar_futbol"),
 ]
--- proyecto_final/core/views.py
+++ proyecto_final/core/views.py
@@ -6,3 +6,12 @@ def inicio(request):
 
 def mostrar_futbol(request):
     return render(request, "core/mostrar_futbol.html")
+
+def agregar_futbol(request):
+    return render(request, "core/agregar_futbol.html")
+
+def editar_futbol(request):
+    return render(request, "core/editar_futbol.html")
+
+def eliminar_futbol(request):
+    return render(request, "core/eliminar_futbol.html")
