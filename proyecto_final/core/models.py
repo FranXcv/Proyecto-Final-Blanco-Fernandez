e8c95394a753e42417ea3ffbe1404b10334a0184
Franco <blancofranco205@gmail.com>
2023-04-11 11:08:16 -0300

    login y register

--- proyecto_final/core/models.py
+++ proyecto_final/core/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Futbol(models.Model):
@@ -7,3 +8,4 @@ class Futbol(models.Model):
 
     def __str__(self):
         return f"{self.id} - {self.nombre} - {self.nro_equipos}"
+    
--- proyecto_final/core/urls.py
+++ proyecto_final/core/urls.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include 
-from core.views import inicio, mostrar_futbol, editar_futbol, eliminar_futbol, agregar_futbol
+from core.views import inicio, FormularioFutbol, editar_futbol, eliminar_futbol, agregar_futbol
+from django.contrib.auth.views import LogoutView
+from core import views
+
 urlpatterns = [
     path("", inicio, name="index"),
-    path("mostrar_futbol/", mostrar_futbol, name="mostrar_futbol"),
+    path("mostrar_futbol/", FormularioFutbol, name="mostrar_futbol"),
     path("agregar_futbol/", agregar_futbol, name="agregar_futbol"),
     path("editar_futbol/", editar_futbol, name="editar_futbol"),
-    path("eliminar_futbol/", eliminar_futbol, name="eliminar_futbol"),
+    path("eliminar_futbol/<int:id_futbol>", eliminar_futbol, name="eliminar_futbol"),
+    path("login", views.login_request, name= "login"),
+    path("register", views.register, name="Register"),
+    path("logout", LogoutView.as_view(template_name="core/logout.html"), name= "Logout"),
 ]
